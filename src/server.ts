aae3d8a8a9c8b351387e84bceabbfb069b89e7bd
marcosR-silva <marcos.1998226@gmail.com>
2021-06-12 20:41:31 -0300

    structuring controller layer

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import './database';
+import { routes } from './routes';
 
 import * as dotenv from 'dotenv';
 dotenv.config();
@@ -9,13 +10,6 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 
-app.get('/', (request, response) => { 
-    return response.status(200).json({ message:'Ola Mundo!!' })
-});
-
-app.post('/user', (request, response) => {
-    const { name } = request.body;
-    return response.json({ message: `Usuário salvo com sucesso! ${ name }`})
-});
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
